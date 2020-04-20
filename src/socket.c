0738de8e39351dfbc48e1b00d8984b78ac94c96f
ntucker2 <ntucker2@nd.edu>
2020-04-20 13:41:16 -0400

    socket.c - completed

--- src/socket.c
+++ src/socket.c
@@ -20,15 +20,44 @@
  **/
 int socket_listen(const char *port) {
     /* Lookup server address information */
+    struct addrinfo *results;
+    struct addrinfo hints = {
+        .ai_family = AF_UNSPEC,     // IPv4 or IPv6
+        .ai_socktype = SOCK_STREAM, // TCP
+        .ai_flags = AI_PASSIVE,     // Listen on socket
+    };
+
+    int status = getaddrinfo(NULL, port, &hints, &results)
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(status));
+        return -1;
+    }
 
     /* For each server entry, allocate socket and try to connect */
-    int socket_fd = -1;
-    for (struct addrinfo *p = results; p != NULL && socket_fd < 0; p = p->ai_next) {
+    int server_fd = -1;
+    for (struct addrinfo *p = results; p != NULL && server_fd < 0; p = p->ai_next) {
 	/* Allocate socket */
+        server_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if(server_fd < 0) {
+            fprintf(stderr, "Unable to make socket: %s\n", strerror(errno));
+            continue;
+        }
 
 	/* Bind socket */
+        if(bind(server_fd, p->ai_addr, p->ai_addrlen) < 0) {
+            fprintf(stderr, "Unable to bind: %s\n", strerror(errno));
+            close(server_fd);
+            server_fd = -1;
+            continue;
+        }
 
     	/* Listen to socket */
+        if(listen(server_fd, SOMAXCONN) < 0) {
+            fprintf(stderr, "Unable to listen: %s\n", strerror(errno));
+            close(server_fd);
+            server_fd = -1;
+            continue;
+        }
     }
 
     freeaddrinfo(results);
