9ab8b3aac776f4c19a3a171b7f747b2a2b14835a
ntucker2 <ntucker2@nd.edu>
2020-04-22 02:16:47 -0400

    single.c is complete

--- src/single.c
+++ src/single.c
@@ -7,7 +7,7 @@
 
 #include <unistd.h>
 
-/**
+/** DONE
  * Handle one HTTP request at a time.
  *
  * @param   sfd         Server socket file descriptor.
@@ -17,10 +17,17 @@ int single_server(int sfd) {
     /* Accept and handle HTTP request */
     while (true) {
     	/* Accept request */
+        Request *request = accept_request(sfd);
+        if(!request){
+            log("Unable to accept request: %s", strerror(errno));
+            continue;
+        }
 
 	/* Handle request */
+        handle_request(request);
 
 	/* Free request */
+        free_request(request);
     }
 
     /* Close server socket */
